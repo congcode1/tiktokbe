ff583eb8b689fda1f6a949062342195f31a7a40f
Ng Thanh Cong <nxgthanhcong@gmail.com>
2022-05-08 19:53:17 +0700

    test husky

--- src/app/controllers/VideoController.js
+++ src/app/controllers/VideoController.js
@@ -1,12 +1,11 @@
-
 class VideoController {
     index(req, res, next) {
-        res.render("video/index")
+        res.render('video/index');
     }
     create(req, res, next) {
-        console.log(req.body)
-        res.send("create video api")
+        console.log(req.body);
+        res.send('create video api');
     }
 }
 
-export default new VideoController()
+export default new VideoController();
